177915a69af677271fe83df2d40a5c9a622714ba
alejo-RB <rodrgueza@yahoo.es>
2023-05-11 00:53:02 -0500

    update portafolio-arb

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,17 +1,52 @@
 import React from "react";
-import Navbar from './Navbar' 
+import Navbar from "./Navbar";
 import styled from "styled-components";
 
 const Section = styled.div`
   height: 100vh;
   scroll-snap-align: center;
-`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: space-between;
+
+  scroll-snap-type: y mandatory;
+  scroll-behavior: smooth;
+  overflow-y: auto;
+  scrollbar-width: none;
+`;
+
+const Left = styled.div`
+  height: 100vh;
+  scroll-snap-align: center;
+  width: 1200px;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Right = styled.div`
+  height: 100vh;
+  scroll-snap-align: center;
+  width: 1200px;
+  display: flex;
+  justify-content: space-between;
+`;
+
+const Container = styled.div`
+  height: 100vh;
+  scroll-snap-align: center;
+  width: 1200px;
+  display: flex;
+  justify-content: space-between;
+`;
 
 const Hero = () => {
-  return(
-  <Section>
-    Hero
-  </Section>);
+  return (
+    <Section>
+      <Navbar />
+      <Container>Hero</Container>
+    </Section>
+  );
 };
 
 export default Hero;
