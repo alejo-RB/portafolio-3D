ee984dfc3a6af89c1180d5859e5f54a6a030072c
alejo-RB <rodrgueza@yahoo.es>
2023-05-10 23:14:20 -0500

    update portafolio-arb

---
+++ src/components/Hero.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Navbar from './Navbar' 
+import styled from "styled-components";
+
+const Section = styled.div`
+  height: 100vh;
+  scroll-snap-align: center;
+`
+
+const Hero = () => {
+  return(
+  <Section>
+    Hero
+  </Section>);
+};
+
+export default Hero;
