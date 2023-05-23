22f93301900da6b27f05ac00094e2ad8ee835b97
alejo-RB <rodrgueza@yahoo.es>
2023-05-22 19:52:51 -0500

    update

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/portafolio-3D/"
+  base: "/portafolio-3d/"
 })
